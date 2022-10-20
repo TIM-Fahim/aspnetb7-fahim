6f94094b0439651d7fe6405020d52b47ff074d30
T.I.M. Fahim Sahryer <fahimsahryer@gmail.com>
2022-10-21 01:30:46 +0600

    1

---
+++ Assignment02/Watermelon/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+int input = Convert.ToInt32(Console.ReadLine());
+
+if (input % 2 == 0)
+{
+    int pice = input / 2;
+    if (pice % 2 == 0)
+    {
+        Console.WriteLine("YES");
+    }
+    else
+    {
+        Console.WriteLine("NO");
+    }
+}
+else
+{
+    Console.WriteLine("NO");
+}
