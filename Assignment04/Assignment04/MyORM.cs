2ef2954d27db7e1b702069d52ef2fe344026ebcc
T.I.M. Fahim Sahryer <fahimsahryer@gmail.com>
2022-10-23 20:40:24 +0600

    1

---
+++ Assignment04/Assignment04/MyORM.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment04
+{
+    public class MyORM<G, T>
+    {
+        public void Insert(T item)
+        { 
+        
+        }
+        public void Update(T item)
+        {
+
+        }
+        public void Delete(T item)
+        {
+
+        }
+        public void Delete(G id)
+        { 
+        
+        }
+        public Course GetById(G id)
+        {
+            return new Course();
+;        }
+        public List<Course> GetAll()
+        {
+            return null;
+        }
+    }
+}
--- Assignment04/Assignment04/Program.cs
+++ Assignment04/Assignment04/Program.cs
@@ -1,6 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-string input = "AAAABBBCCCCDRRR";
-
+﻿
 
