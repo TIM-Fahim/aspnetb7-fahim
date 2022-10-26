5bf2b736bb66ea441a66e3c3505087c63904fc7c
T.I.M. Fahim Sahryer <fahimsahryer@gmail.com>
2022-10-26 15:20:45 +0600

    5

---
+++ Assignment02/Beautiful Matrix/Program.cs
@@ -0,0 +1,20 @@
+﻿
+int[,] matrix = new int[5,5];
+
+int answer = 0; 
+
+for (int i = 0; i < 5; i++)
+{
+    string[] input = (Console.ReadLine().Split(' '));
+
+    for (int j = 0; j < 5; j++)
+    {
+        matrix[i, j] = int.Parse(input[j]);
+        if (matrix[i, j] == 1)
+        {
+            answer= (Math.Abs(i - 2) + Math.Abs(j - 2));
+            
+        }
+    }
+}
+Console.WriteLine(answer);
---
+++ Assignment02/Bit++/Program.cs
@@ -0,0 +1,18 @@
+﻿int input = int.Parse(Console.ReadLine());
+
+int x = 0;
+
+for (int i = 0; i < input; i++)
+{
+    string operation = Console.ReadLine();
+    if (operation.Contains("++"))
+    {
+        x++;
+    }
+    else
+    {
+        x--;
+    }
+}
+
+Console.WriteLine(x);
