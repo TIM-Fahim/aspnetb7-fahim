5e3664b6cff65d2e23c9ecf7e5c2bc73325c56fc
T.I.M. Fahim Sahryer <fahimsahryer@gmail.com>
2022-10-26 12:05:30 +0600

    4 done

---
+++ Assignment02/Domino piling/Program.cs
@@ -0,0 +1,7 @@
+﻿string input = Console.ReadLine();
+
+string[] inputArray = input.Split(' ');
+
+int boardSize = int.Parse(inputArray[0]) * int.Parse(inputArray[1]);
+
+Console.WriteLine(boardSize / 2);
