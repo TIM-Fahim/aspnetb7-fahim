8c9d535368f59a6c0d8f270a78edbb3ae0fe028e
T.I.M. Fahim Sahryer <fahimsahryer@gmail.com>
2022-10-27 09:50:57 +0600

    More

---
+++ Assignment02/Longest Common Prefix/Program.cs
@@ -0,0 +1,28 @@
+﻿//LeetCode
+
+string[] input = { "flower", "flow", "flight" };
+
+string answer = "";
+
+string LongestCommonPrefix(string[] strs)
+{
+    if (strs.Length == 0 || strs == null)
+    {
+        return "";
+    }
+    else if (strs.Length == 1)
+    {
+        return strs[0];
+    }
+    
+    for (int i = 0; i < input.Length; i++)
+    {
+        string temp ="";
+        for (int j = 0; j < strs[i].Length; j++)
+        {
+            temp = strs[i].Substring(0,0);
+        }
+    }
+
+    return null;
+}
