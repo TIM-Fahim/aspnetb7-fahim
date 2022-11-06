a11aa6e06d4493f791bc081f9aa978804017cea0
T.I.M. Fahim Sahryer <fahimsahryer@gmail.com>
2022-11-06 14:35:10 +0600

    23

---
+++ Assignment02/Merge Two Sorted Lists/Program.cs
@@ -0,0 +1,64 @@
+﻿// LeetCode
+using Merge_Two_Sorted_Lists;
+
+ListNode l10 = new ListNode(1);
+ListNode l11 = new ListNode(2);
+l10.next = l11;
+ListNode l12 = new ListNode(4);
+l11.next = l12;
+
+ListNode l20 = new ListNode(1);
+ListNode l21 = new ListNode(3);
+l20.next = l21;
+ListNode l22 = new ListNode(4);
+l21.next = l22;
+
+ListNode l3 = MergeTwoLists(l10, l20);
+
+while (l3 != null)
+{
+    Console.WriteLine(l3.val);
+    l3 = l3.next;
+}
+
+ListNode MergeTwoLists(ListNode list1, ListNode list2)
+{
+    if (list1 == null || list2 == null)
+    {
+        if (list1 == null)
+        {
+            return list2;
+        }
+        else
+        {
+            return list1;
+        }
+    }
+    ListNode dummyHead = new ListNode(0);
+    ListNode current = dummyHead;
+
+    while (list1 != null && list2 != null)
+    {
+        if (list1.val <= list2.val)
+        {
+            current.next = list1;
+            list1 = list1.next;
+        }
+        else
+        {
+            current.next = list2;
+            list2 = list2.next;
+        }
+        current = current.next;
+    }
+    if (list1 != null)
+    {
+        current.next = list1;
+    }
+    if (list2 != null)
+    {
+        current.next = list2;
+    }
+
+    return dummyHead.next;
+}
---
+++ Assignment02/Remove Duplicates from Sorted Array/Program.cs
@@ -0,0 +1,24 @@
+﻿//LeetCode
+int[] nums = new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+int length = RemoveDuplicates(nums);
+Console.WriteLine(length);
+
+int RemoveDuplicates(int[] nums)
+{
+    int count = 1;
+    for (int i = 1; i < nums.Length; i++)
+    {
+        if (nums[i] != nums[i - 1])
+        {
+            nums[count] = nums[i];
+            count++;
+        }
+    }
+    if (nums == null || nums.Length ==0)
+    {
+        return 0;
+    }
+
+    return count;
+    
+}
