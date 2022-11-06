d648d717c108ccac9bdbd6ccd52a44acdf84e0c4
T.I.M. Fahim Sahryer <fahimsahryer@gmail.com>
2022-11-06 12:32:20 +0600

    22

---
+++ Assignment02/Valid Parentheses/Program.cs
@@ -0,0 +1,45 @@
+﻿//Leet Code
+string s = "(]";
+
+Console.WriteLine(IsValid(s));
+
+bool IsValid(string s)
+{
+    Stack<char> stack = new Stack<char>();
+
+    foreach (char c in s)
+    {
+        if (c == '(' || c == '[' || c == '{')
+        {
+            stack.Push(c);
+        }
+        else
+        {
+            if (stack.Count == 0)
+            {
+                return false;
+            }
+            char top = stack.Pop();
+            if (c == ')' && top != '(')
+            {
+                return false;
+            }
+            if (c == ']' && top != '[')
+            {
+                return false;
+            }
+            if (c == '}' && top != '{')
+            {
+                return false;
+            }
+        }
+    }
+    if (stack.Count == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
