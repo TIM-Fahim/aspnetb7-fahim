2b575290d808961a5ed35196bc1ee9300804f185
T.I.M. Fahim Sahryer <fahimsahryer@gmail.com>
2022-11-12 03:52:19 +0600

    36

---
+++ Assignment02/Unique Morse Code Words/Program.cs
@@ -0,0 +1,24 @@
+﻿// LeetCode
+using System.Text;
+string[] s = new string[] { "gin", "zen", "gig", "msg" };
+Console.WriteLine(UniqueMorseRepresentations(s));
+int UniqueMorseRepresentations(string[] words)
+{
+    string[] codes = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.." };
+
+    HashSet<string> set = new HashSet<string>();
+
+    StringBuilder stBuild = new StringBuilder();
+
+    for (int i = 0; i < words.Length; i++)
+    {
+        for (int j = 0; j < words[i].Length; j++)
+        {
+            stBuild.Append(codes[words[i][j] - 'a']); 
+        }
+        set.Add(stBuild.ToString());
+        stBuild.Clear();
+    }
+
+    return set.Count;
+}
