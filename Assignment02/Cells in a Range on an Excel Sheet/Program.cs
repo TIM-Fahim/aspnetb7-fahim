98a2b4e2f33ef361ff9c8cf69c3fe710c37181b1
T.I.M. Fahim Sahryer <fahimsahryer@gmail.com>
2022-11-12 02:26:50 +0600

    32

---
+++ Assignment02/Cells in a Range on an Excel Sheet/Program.cs
@@ -0,0 +1,30 @@
+﻿// LeetCode
+using System.Collections;
+
+string s = "A1:F1";
+
+IList<string> ans = CellsInRange(s);
+IList<string> CellsInRange(string s)
+{
+    char[] str = s.ToCharArray();
+
+    
+    List<string> result = new List<string>();
+
+    char col1 = str[0];
+    char col2 = str[3];
+    char row1 = str[1];
+    char row2 = str[4];
+
+    for (char i = col1; i <= col2; i++)
+    {
+        for (char j = row1; j <= row2; j++)
+        {
+            result.Add(i.ToString() + j.ToString());
+            Console.WriteLine(i.ToString() + j.ToString());
+        }
+    }
+
+
+    return result;
+}
