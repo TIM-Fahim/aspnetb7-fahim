19c0fb82f21f34b67f53e99e04052289081ae373
T.I.M. Fahim Sahryer <fahimsahryer@gmail.com>
2022-11-12 13:36:22 +0600

    40

---
+++ Assignment02/Length of Last Word/Program.cs
@@ -0,0 +1,19 @@
+﻿// LeetCode
+Console.WriteLine(LengthOfLastWord("Hello World"));
+int LengthOfLastWord(string s)
+{
+    int count = 0;
+    string word = "";
+    for (int i = s.Length - 1; i >= 0; i--)
+    {
+        if (s[i] != ' ')
+        {
+            if (count > 0)
+                return count;
+        }
+        else
+            count++;
+    }
+    //string ans = $"The last word is \"{word}\" with the length of {count}";
+    return count;
+}
