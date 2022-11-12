ca43841a48e79f4b50eccbb3aa35de239dfc95ab
T.I.M. Fahim Sahryer <fahimsahryer@gmail.com>
2022-11-12 12:59:52 +0600

    39

---
+++ Assignment02/Check if the Sentence Is Pangram/Program.cs
@@ -0,0 +1,23 @@
+﻿// LeetCode
+
+Console.WriteLine(CheckIfPangram("thequickbrownfoxjumpsoverthelazydog"));
+bool CheckIfPangram(string sentence)
+{
+    if (sentence.Length < 26)
+    {
+        return false;
+    }
+    else
+    {
+        var charArray = sentence.ToCharArray();
+        var distinctChars = charArray.Distinct().ToArray();
+        if (distinctChars.Length == 26)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
