3978c0054678e0ca33f1d910dea9e1d852e04eaa
T.I.M. Fahim Sahryer <fahimsahryer@gmail.com>
2022-11-12 12:32:57 +0600

    38

---
+++ Assignment02/Check If Two String Arrays are Equivalent/Program.cs
@@ -0,0 +1,24 @@
+﻿// LeetCode
+
+string[] w1 = { "ab", "c" }; string[] w2 = { "a", "bc" };
+Console.WriteLine(ArrayStringsAreEqual(w1,w2));
+bool ArrayStringsAreEqual(string[] word1, string[] word2)
+{
+    bool ans = true;
+
+    string s1 = string.Join("", word1);
+    Console.WriteLine(s1);
+    
+    string s2 = string.Join("", word2);
+
+    Console.WriteLine(s2);
+    if(s1==s2)
+    {
+        ans = true;
+    }
+    else
+    {
+        ans = false;
+    }
+    return ans;
+}
