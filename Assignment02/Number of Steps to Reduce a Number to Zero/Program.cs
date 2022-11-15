641e784884a52e5ab7ad9037f26baf002a83d02a
T.I.M. Fahim Sahryer <fahimsahryer@gmail.com>
2022-11-15 13:02:47 +0600

    LeetCode 30

---
+++ Assignment02/Number of Steps to Reduce a Number to Zero/Program.cs
@@ -0,0 +1,42 @@
+﻿// LeetCode
+Console.WriteLine(NumberOfSteps(14));
+int NumberOfSteps(int num)
+{
+    int steps = 0;
+
+    while (num > 0)
+    {
+        if (num % 2 == 0)
+        {
+            num /= 2;  
+        }
+        else
+        {
+            num -= 1;
+        }
+
+        steps++;
+    }
+    return steps;
+}
+
+int NumberOfStepsBti(int num)
+{
+    int steps = 0;
+    
+    while (num > 0)
+    {
+        if ((num & 1) == 0)
+        {
+            //num /= 2;
+            num >>= 1;
+        }
+        else
+        {
+            num -= 1;
+        }
+
+        steps++;
+    }
+    return steps;
+}
