4cbe917c82bfb0071a231d6a185ba46e996c0021
T.I.M. Fahim Sahryer <fahimsahryer@gmail.com>
2022-11-15 12:34:56 +0600

    LeetCode 28

---
+++ Assignment02/Richest Customer Wealth/Program.cs
@@ -0,0 +1,23 @@
+﻿// LeetCode
+
+
+int MaximumWealth(int[][] accounts)
+{
+    int Richest = 0;
+
+    foreach (int[] account in accounts)
+    {
+        int Wealth = 0;
+
+        foreach (int money in account)
+        {
+            Wealth += money;
+        }
+
+        if (Wealth > Richest)
+        {
+            Richest = Wealth;
+        }
+    }
+    return Richest;
+}
