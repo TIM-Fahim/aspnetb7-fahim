e6501e571b6adb63414b962118a9327487495353
T.I.M. Fahim Sahryer <fahimsahryer@gmail.com>
2022-11-15 12:29:03 +0600

    leetcode 27

---
+++ Assignment02/Running Sum of 1d Array/Program.cs
@@ -0,0 +1,19 @@
+﻿// LeetCode
+
+
+int[] nums = { 1, 2, 3, 4 };
+
+int[] result = RunningSum(nums);
+
+foreach (int i in result)
+{
+    Console.WriteLine(i);
+}
+int[] RunningSum(int[] nums)
+{
+    for (int i = 1; i < nums.Length; i++)
+    {
+        nums[i] = nums[i] + nums[i - 1];
+    }
+    return nums;
+ }
