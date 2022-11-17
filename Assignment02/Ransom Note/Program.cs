6d6670e1ffbde68057d1e2a39496443211542d18
T.I.M. Fahim Sahryer <fahimsahryer@gmail.com>
2022-11-17 14:53:49 +0600

    33

---
+++ Assignment02/Ransom Note/Program.cs
@@ -0,0 +1,36 @@
+﻿// LeetCode
+
+bool CanConstruct(string ransomNote, string magazine)
+{
+    var dict = new Dictionary<char, int>();
+    foreach (var c in magazine)
+    {
+        if (dict.ContainsKey(c))
+        {
+            dict[c]++;
+        }
+
+        else
+        {
+            dict.Add(c, 1);
+        }
+    }
+
+    foreach (var c in ransomNote)
+    {
+        if (dict.ContainsKey(c))
+        {
+            dict[c]--;
+            if (dict[c] == 0)
+            {
+                dict.Remove(c);
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
