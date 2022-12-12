ed4acf5fcb6659033c4369e8201060795617f4a6
T.I.M. Fahim Sahryer <fahimsahryer@gmail.com>
2022-12-12 13:23:35 +0600

    37

---
+++ Assignment02/Valid Anagram/Program.cs
@@ -0,0 +1,24 @@
+﻿bool IsAnagram(string s, string t)
+{
+    var first = s.OrderBy(c => c);
+    var second = t.OrderBy(c => c);
+    return first.SequenceEqual(second);
+}
+
+bool IsAnagram2(string s, string t)
+{
+    string first = String.Concat(s.OrderBy(c => c));
+    //Console.WriteLine(first);
+    //Contact replace string
+    string second = String.Concat(t.OrderBy(c => c));
+
+    return first == second;
+}
+
+IsAnagram2("anagram", "nagaram");
+
+//string f = "fahim";
+//string g = "sahryer";
+
+//string first = String.Concat(g);
+//Console.WriteLine(first);
