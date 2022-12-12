ae02e95c6ce62539ca6e8a1c97a769efef523852
T.I.M. Fahim Sahryer <fahimsahryer@gmail.com>
2022-12-12 13:04:55 +0600

    36

---
+++ Assignment02/Climbing Stairs/Program.cs
@@ -0,0 +1,15 @@
+﻿int ClimbStairs(int n)
+{
+    int first = 1;
+    int second = 1;
+    int temp = 0; // 0 is the default value for int
+    for (int i = 2; i <= n; i++)
+    {
+        temp = first;
+        first += second;
+        second = temp;
+    }
+    return first;
+}
+
+Console.WriteLine(ClimbStairs(5));
