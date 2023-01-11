17e100b039a5ccce5fd76aa92b600a1ccef221d6
T.I.M. Fahim Sahryer <fahimsahryer@gmail.com>
2023-01-11 10:29:03 +0600

    36

---
+++ Assignment02/Single Number/Program.cs
@@ -0,0 +1,14 @@
+﻿
+int SingleNumber(int[] nums)
+{
+    int result = 0;
+    foreach (int num in nums)
+    {
+        result ^= num;
+    }
+    return result;
+}
+
+int[] nums = { 4, 1, 2, 1, 2 };
+
+Console.WriteLine(SingleNumber(nums));
