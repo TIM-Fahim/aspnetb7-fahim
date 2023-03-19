0b8a8fb2045da2e4fa5b6236e1cc1435eb064526
T.I.M. Fahim Sahryer <fahimsahryer@gmail.com>
2023-03-19 17:28:55 +0600

    2

--- School/School.Infrastructure/Entities/Student.cs
+++ School/School.Infrastructure/Entities/Student.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace School.Infrastructure.Entities
 {
-    internal class Student
+    public class Student
     {
+        int ID { get; set; }
+        string Name { get; set; }
+
+        DateOnly DateOfBirth { get; set; }
+        string? Email { get; set; }
+        string PhoneNumber { get; set; }
+        string Address { get; set; }
+
+        decimal CGPA { get; set; }
+
     }
 }
