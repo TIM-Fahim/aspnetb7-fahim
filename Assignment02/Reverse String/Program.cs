a4d2520d34c7b3b73d86df6007d69f6b15fa51c6
T.I.M. Fahim Sahryer <fahimsahryer@gmail.com>
2023-05-27 23:16:17 +0600

    one more

---
+++ Assignment02/Reverse String/Program.cs
@@ -0,0 +1,17 @@
+﻿void ReverseString(char[] s)
+{
+    int j = s.Length - 1;
+    for (int i = 0; i < s.Length / 2; i++)
+    {
+        char temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+        j--;
+    }
+}
+
+string s = "hello";
+char[] c = s.ToCharArray();
+ReverseString(c);
+
+Console.WriteLine(c);
