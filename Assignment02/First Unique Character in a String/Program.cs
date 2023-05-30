57322f9407cba3dab69f1ef6c77cee6f63a37477
T.I.M. Fahim Sahryer <fahimsahryer@gmail.com>
2023-05-30 17:20:25 +0600

    new one

---
+++ Assignment02/First Unique Character in a String/Program.cs
@@ -0,0 +1,27 @@
+﻿int FirstUniqChar(string s)
+{
+
+
+    Dictionary<char, int> dict = new Dictionary<char, int>();
+
+    for (int i = 0; i < s.Length; i++)
+    {
+        if (dict.ContainsKey(s[i]))
+        {
+            dict[s[i]]++;
+        }
+        else
+        {
+            dict.Add(s[i], 1);
+        }
+    }
+
+    for (int i = 0; i < s.Length; i++)
+    {
+        if (dict[s[i]] == 1)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
